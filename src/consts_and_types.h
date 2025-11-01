a8a196cb93810bce665a48167e6f0a3d40911e1f
kumi292 <ryoyw.3531@gmail.com>
2025-11-01 17:31:36 +0900

    定数と型の定義追加/変更

--- src/consts_and_types.h
+++ src/consts_and_types.h
@@ -1,8 +1,11 @@
 #pragma once
 
+#include <tuple>
 #include <vector>
 
 using NumType = long long;
-using SharesType = std::vector<NumType>;
-constexpr int PARTY_COUNT = 2;
-constexpr int MODULUS = 1 << 30;
+using ShareType = std::tuple<NumType, NumType>;
+using SharesType = std::vector<ShareType>;
+constexpr int PARTY_COUNT = 3;
+constexpr int ACCESS_COUNT = 2;
+constexpr NumType MODULUS = (1LL << 40);
