a59a6b6b906312b07fb522768211024a03a88680
kumi292 <ryoyw.3531@gmail.com>
2025-11-01 22:25:43 +0900

    合同式の法を修正および乱数の上限を設定

--- src/consts_and_types.h
+++ src/consts_and_types.h
@@ -8,4 +8,5 @@ using ShareType = std::tuple<NumType, NumType>;
 using SharesType = std::vector<ShareType>;
 constexpr int PARTY_COUNT = 3;
 constexpr int ACCESS_COUNT = 2;
-constexpr NumType MODULUS = (1LL << 40);
+constexpr NumType MODULUS = 1LL << 28;
+constexpr NumType RANDOM_MAX = 1LL << 29;
