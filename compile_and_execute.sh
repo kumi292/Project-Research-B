bd2d6472f12a00942efeed86dbfab58d2589b976
kumi292 <ryoyw.3531@gmail.com>
2025-11-26 19:54:21 +0900

    出力する文字を変更

--- compile_and_execute.sh
+++ compile_and_execute.sh
@@ -1,10 +1,16 @@
 #!/bin/zsh
 if [ $1 = "main" ]; then
   echo "mainを実行します"
+  echo "> clang++ -std=c++17 -Iinclude src/*.cpp -o bin/main"
   clang++ -std=c++17 -Iinclude src/*.cpp -o bin/main
+  echo "> bin/main"
+  echo ""
   bin/main
 else
   echo "$1のテストをします"
+  echo "> clang++ -std=c++17 -Iinclude {tests/test_,src/}$1.cpp -o bin/$1"
   clang++ -std=c++17 -Iinclude {tests/test_,src/}$1.cpp -o bin/$1
+  echo "> bin/$1"
+  echo ""
   bin/$1
 fi
