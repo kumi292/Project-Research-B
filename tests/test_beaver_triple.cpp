117c5ada84f8e64ac2a53175224904c7026b11aa
kumi292 <ryoyw.3531@gmail.com>
2025-11-26 21:27:49 +0900

    乗算のテストを追加

--- tests/test_beaver_triple.cpp
+++ tests/test_beaver_triple.cpp
@@ -18,6 +18,7 @@ int main() {
   std::cout << "Party 1 Share: " << shares_a[0] << std::endl;
   std::cout << "Party 2 Share: " << shares_a[1] << std::endl;
   std::cout << std::endl;
+
   std::cout << "Number B: \n";
   set_number_from_stdin(plain_num_b);
   SharesType shares_b = BT::create_shares(plain_num_b);
@@ -25,8 +26,10 @@ int main() {
   std::cout << "Party 1 Share: " << shares_b[0] << std::endl;
   std::cout << "Party 2 Share: " << shares_b[1] << std::endl;
   std::cout << std::endl;
+
   SharesType shares_party_1 = {shares_a[0], shares_b[0]};
   SharesType shares_party_2 = {shares_a[1], shares_b[1]};
+
   SharesType shares_added = BT::add({shares_party_1, shares_party_2});
   std::cout << "[Local Addition]" << std::endl;
   std::cout << "Party 1: " << shares_party_1[0] << " + " << shares_party_1[1]
@@ -38,5 +41,14 @@ int main() {
             << BT::reconstruct_from_shares(shares_added) << std::endl;
   std::cout << "Sum of numbers (not using secret sharing scheme) is "
             << plain_num_a + plain_num_b << std::endl;
+  std::cout << std::endl;
+
+  SharesType shares_multiplied = BT::multiply({shares_party_1, shares_party_2});
+  std::cout << "[Multiplication]" << std::endl;
+  std::cout << "Product of numbers (using beaver triple) is "
+            << BT::reconstruct_from_shares(shares_multiplied) << std::endl;
+  std::cout << "Product of numbers (not using secret sharing scheme) is "
+            << plain_num_a * plain_num_b << std::endl;
+
   return 0;
 }
