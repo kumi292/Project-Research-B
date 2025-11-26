0021579a7b0cab2d6207258fc9d52ef2e93ddf93
kumi292 <ryoyw.3531@gmail.com>
2025-11-26 21:25:15 +0900

    定数の定義の追加

--- include/beaver_triple.h
+++ include/beaver_triple.h
@@ -6,6 +6,8 @@ using NumType = long long;
 using SharesType = std::vector<NumType>;
 constexpr int PARTY_COUNT = 2;
 constexpr NumType MODULUS = 1LL << 28;
+constexpr NumType RANDOM_MAX = 1LL << 29;
+constexpr NumType TRIPLE_MAX = 1LL << 13;
 
 NumType mod(NumType dividend, NumType divisor);
 
@@ -14,5 +16,6 @@ namespace BT {
 SharesType create_shares(NumType plain_num);
 NumType reconstruct_from_shares(SharesType shares);
 SharesType add(std::vector<SharesType> parties_with_shares);
+SharesType multiply(std::vector<SharesType> parties_with_shares);
 
 } // namespace BT
