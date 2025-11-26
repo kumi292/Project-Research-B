c8b0c3c76702f46ea208cef1529c8fb6d38588d3
kumi292 <ryoyw.3531@gmail.com>
2025-11-26 21:26:37 +0900

    乱数の生成にメルセンヌツイスタを使う用変更

--- src/beaver_triple.cpp
+++ src/beaver_triple.cpp
@@ -16,17 +16,19 @@ SharesType create_shares(NumType plain_num) {
   if (plain_num >= MODULUS)
     exit(1);
   SharesType shares(PARTY_COUNT);
-  std::random_device random_generater;
+  std::random_device seed_gen;
+  std::mt19937 engine(seed_gen());
+  std::uniform_int_distribution<NumType> dist(0, RANDOM_MAX);
   NumType mod_of_plain_num = mod(plain_num, MODULUS);
   NumType mod_of_shares_sum = 0;
   for (int share_i = 0; share_i < PARTY_COUNT; share_i++) {
     if (share_i + 1 == PARTY_COUNT) {
-      NumType random_num = random_generater();
+      NumType random_num = dist(engine);
       shares[share_i] =
           random_num +
           (mod_of_plain_num - (mod_of_shares_sum + mod(random_num, MODULUS)));
     } else {
-      shares[share_i] = random_generater();
+      shares[share_i] = dist(engine);
       mod_of_shares_sum =
           mod(mod_of_shares_sum + mod(shares[share_i], MODULUS), MODULUS);
     }
