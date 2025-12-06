877c0b7fb1cb5c182880e9c17125a612e02e1891
kumi292 <ryoyw.3531@gmail.com>
2025-12-06 23:29:16 +0900

    コンパイルに失敗したら実行ファイルを実行しないように変更

--- compile_and_execute.sh
+++ compile_and_execute.sh
@@ -1,16 +1,21 @@
 #!/bin/zsh
 if [ $1 = "main" ]; then
-  echo "mainを実行します"
+  echo "executing main..."
   echo "> clang++ -std=c++17 -Iinclude src/*.cpp -o bin/main"
   clang++ -std=c++17 -Iinclude src/*.cpp -o bin/main
-  echo "> bin/main"
-  echo ""
-  bin/main
 else
-  echo "$1のテストをします"
+  echo "testing $1..."
   echo "> clang++ -std=c++17 -Iinclude {tests/test_,src/}$1.cpp -o bin/$1"
   clang++ -std=c++17 -Iinclude {tests/test_,src/}$1.cpp -o bin/$1
+fi
+
+exit_code=$?
+
+if [ $exit_code -eq 0 ]; then
   echo "> bin/$1"
   echo ""
   bin/$1
-fi
+else
+  echo "failed in compiling (status: $exit_code)"
+  exit $exit_code
+fi  
