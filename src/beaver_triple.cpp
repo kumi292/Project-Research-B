0dd3a85439e3318e76c8a2e055016cf0cae845ae
kumi292 <ryoyw.3531@gmail.com>
2025-12-06 21:29:38 +0900

    内積の処理を追加

--- src/beaver_triple.cpp
+++ src/beaver_triple.cpp
@@ -91,4 +91,25 @@ SharesType multiply(std::vector<SharesType> parties_with_shares) {
   return multiplied_shares;
 }
 
+// { {veca_component1_sharei, veca_component2_sharei,...},
+// {vecb_component1_sharei, vecb_component2_sharei,...} }
+SharesType inner_product(std::vector<SharesType> party1_shares,
+                         std::vector<SharesType> party2_shares) {
+  int vec_size = party1_shares[0].size();
+  std::vector<SharesType> multiplied_shares_vec;
+  for (int component_i = 0; component_i < vec_size; component_i++) {
+    multiplied_shares_vec.push_back(multiply(
+        {{party1_shares[0][component_i], party1_shares[1][component_i]},
+         {party2_shares[0][component_i], party2_shares[1][component_i]}}));
+  }
+
+  SharesType calculated_shares = {0, 0};
+  for (int component_i = 0; component_i < vec_size; component_i++) {
+    calculated_shares =
+        add({calculated_shares, multiplied_shares_vec[component_i]});
+  }
+
+  return calculated_shares;
+}
+
 } // namespace BT
