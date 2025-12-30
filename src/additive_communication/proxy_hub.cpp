d88ac19d94501ad5498b842d1ba10f6076269a97
kumi292 <ryoyw.3531@gmail.com>
2025-12-30 18:36:23 +0900

    proxy_hubのひな形生成

---
+++ src/additive_communication/proxy_hub.cpp
@@ -0,0 +1,14 @@
+#include "json.hpp"
+#include "zmq.hpp"
+#include <iostream>
+#include <random>
+#include <string>
+
+#include "beaver_triple.h"
+#include "common_functions.h"
+
+using json = nlohmann::json;
+
+int main() {
+  return 0;
+}
