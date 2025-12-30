1d246da89f5556c024546461892096e1c705f7c3
kumi292 <ryoyw.3531@gmail.com>
2025-12-30 18:39:10 +0900

    通信に必要な定数を設定

--- include/common_functions.h
+++ include/common_functions.h
@@ -1,7 +1,17 @@
 #pragma once
 
-using NumType = long long;
 #include <string>
 
 const std::string PORT = "10000";
+const std::string CLIENT = "CLIENT";
+const std::string SERVER_1 = "SERVER 1";
+const std::string SERVER_2 = "SERVER 2";
+const std::string QUERY_SELECT = "QUERY SELECT";
+const std::string QUERY_INSERT = "QUERY INSERT";
+const std::string QUERY_RESULT = "QUERY RESULT";
+const std::string SEND_TRIPLE = "SEND TRIPLE";
+const std::string EXCHANGE_TRIPLE = "EXCHANGE TRIPLE";
+const std::string RETURN_VALUE = "RETURN VALUE";
+
+using NumType = long long;
 NumType mod(NumType dividend, NumType divisor);
