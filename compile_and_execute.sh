e2058cdcd73303b4bfa3872db1a98bfb0153c64e
kumi292 <ryoyw.3531@gmail.com>
2025-12-30 18:19:08 +0900

    実行用シェルスクリプトの変更

--- compile_and_execute.sh
+++ compile_and_execute.sh
@@ -1,21 +1,39 @@
 #!/bin/zsh
-if [ $1 = "main" ]; then
-  echo "executing main..."
-  echo "> clang++ -std=c++17 -Iinclude src/*.cpp -o bin/main"
-  clang++ -std=c++17 -Iinclude src/*.cpp -o bin/main
+if [ $1 = "additive_communication" ]; then
+  echo "executing $2 program..."
+  clang++ -std=c++17 -Iinclude src/beaver_triple.cpp src/additive_communication/$2.cpp src/common_functions.cpp -o bin/$2 -lzmq
+
+  exit_code=$?
+  if [ $exit_code -eq 0 ]; then
+    if [ $2 = "database" ]; then
+      bin/$2 $3
+    else 
+      bin/$2
+    fi
+  else
+    echo "failed in compiling (status: $exit_code)"
+    exit $exit_code
+  fi  
+fi
 else
-  echo "testing $1..."
-  echo "> clang++ -std=c++17 -Iinclude {tests/test_,src/}$1.cpp -o bin/$1"
-  clang++ -std=c++17 -Iinclude {tests/test_,src/}$1.cpp -o bin/$1
+  if [ $1 = "main" ]; then
+    echo "executing main..."
+    echo "> clang++ -std=c++17 -Iinclude src/*.cpp -o bin/main"
+    clang++ -std=c++17 -Iinclude src/*.cpp -o bin/main
+  else
+    echo "testing $1..."
+    echo "> clang++ -std=c++17 -Iinclude {tests/test_,src/}$1.cpp -o bin/$1"
+    clang++ -std=c++17 -Iinclude {tests/test_,src/}$1.cpp -o bin/$1
+  fi
+
+  exit_code=$?
+  if [ $exit_code -eq 0 ]; then
+    echo "> bin/$1"
+    echo ""
+    bin/$1
+  else
+    echo "failed in compiling (status: $exit_code)"
+    exit $exit_code
+  fi  
 fi
 
-exit_code=$?
-
-if [ $exit_code -eq 0 ]; then
-  echo "> bin/$1"
-  echo ""
-  bin/$1
-else
-  echo "failed in compiling (status: $exit_code)"
-  exit $exit_code
-fi  
