df5fe727a10a62e32db12ac098e82941ecb6e569
kumi292 <ryoyw.3531@gmail.com>
2025-12-30 23:18:49 +0900

    定数の追加

--- include/common_functions.h
+++ include/common_functions.h
@@ -9,11 +9,14 @@ const std::string SERVER_2 = "SERVER 2";
 const std::string QUERY_SELECT = "QUERY SELECT";
 const std::string QUERY_INSERT = "QUERY INSERT";
 const std::string QUERY_RESULT = "QUERY RESULT";
+const std::string QUERY_TRUNCATE = "QUERY TRUNCATE";
 const std::string SEND_TRIPLE = "SEND TRIPLE";
 const std::string EXCHANGE_TRIPLE = "EXCHANGE TRIPLE";
 const std::string RETURN_VALUE = "RETURN VALUE";
+const std::string SHUT_DOWN = "SHUT DOWN";
 const std::string RED = "\033[31m";
 const std::string GREEN = "\033[32m";
+const std::string YELLOW = "\033[33m";
 const std::string BLUE = "\033[34m";
 const std::string NO_COLOR = "\033[m";
 
