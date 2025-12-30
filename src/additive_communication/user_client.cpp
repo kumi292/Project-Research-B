708503f96004fb11694307702e64adfe8a63a773
kumi292 <ryoyw.3531@gmail.com>
2025-12-31 01:06:27 +0900

    select文のuser_clientの処理を実装

--- src/additive_communication/user_client.cpp
+++ src/additive_communication/user_client.cpp
@@ -37,6 +37,80 @@ void send_query_insert(zmq::socket_t &sock) {
   send_to_proxy_hub(sock, json_to_send_server_2.dump(2));
 }
 
+void send_query_select(zmq::socket_t &sock) {
+  int db_size = retrieve_db_size();
+  if (db_size == 0) {
+    std::cout << RED << "Cannot execute SELECT query in an empty table."
+              << std::endl;
+    return;
+  }
+
+  // 検索ベクトル作成
+  std::cout << "Enter id to select: ";
+  int id_to_select;
+  std::cin >> id_to_select;
+  if (id_to_select > db_size - 1) {
+    std::cout << RED << "ERROR, inputted number is too big for the table."
+              << NO_COLOR << std::endl;
+    return;
+  }
+  std::vector<int> search_vector(db_size);
+  search_vector[id_to_select] = 1;
+
+  // 検索ベクトルを分散
+  std::vector<NumType> search_vector_share_1(db_size),
+      search_vector_share_2(db_size);
+  for (int id_i = 0; id_i < db_size; id_i++) {
+    SharesType id_i_shares = BT::create_shares(search_vector[id_i]);
+    search_vector_share_1[id_i] = id_i_shares[0];
+    search_vector_share_2[id_i] = id_i_shares[1];
+  }
+
+  // 検索ベクトルのシェアを送信
+  json json_to_send_server_1 = {{"to", SERVER_1},
+                                {"type", QUERY_SELECT},
+                                {"value", search_vector_share_1}};
+  json json_to_send_server_2 = {{"to", SERVER_2},
+                                {"type", QUERY_SELECT},
+                                {"value", search_vector_share_2}};
+
+  // 両サーバーから応答が返ってくるまで待機
+  std::cout << "Waiting for response..." << std::endl;
+  zmq::message_t content_msg;
+  bool is_received_from_server_1 = false;
+  bool is_received_from_server_2 = false;
+  json json_from_server_1, json_from_server_2;
+  while (!is_received_from_server_1 || !is_received_from_server_2) {
+    auto ret = sock.recv(content_msg, zmq::recv_flags::none);
+    if (!ret) {
+      std::cout << "[LOG] " << RED << "ERROR, Can't Receive Massage Correctly."
+                << NO_COLOR << std::endl;
+      return;
+    }
+
+    std::string content = content_msg.to_string();
+    std::cout << "[LOG] " << GREEN << "Received: \n"
+              << NO_COLOR << content << std::endl;
+    auto received_json = json::parse(content);
+    if (received_json["from"] == SERVER_1) {
+      is_received_from_server_1 = true;
+      json_from_server_1 = received_json;
+    } else if (received_json["from"] == SERVER_2) {
+      is_received_from_server_2 = true;
+      json_from_server_2 = received_json;
+    }
+  }
+
+  // 復元して値を取得
+  NumType value_from_server_1 = json_from_server_1["value"];
+  NumType value_from_server_2 = json_from_server_2["value"];
+  std::cout << "Reconstruct from " << value_from_server_1 << " and "
+            << value_from_server_2 << "." << std::endl;
+  std::vector<NumType> result_table = {
+      BT::reconstruct_from_shares({value_from_server_1, value_from_server_2})};
+  print_table(result_table, 1, false, id_to_select);
+}
+
 void send_query_truncate(zmq::socket_t &sock) {
   json generate_truncate_query = {{"type", QUERY_TRUNCATE}};
   send_to_proxy_hub(sock, generate_truncate_query.dump(2));
@@ -46,8 +120,6 @@ void send_request_to_generating_triple(zmq::socket_t &sock) {
   json generate_triple_request = {{"type", SEND_TRIPLE}};
   send_to_proxy_hub(sock, generate_triple_request.dump(2));
 }
-
-void send_query_select(zmq::socket_t &sock) {}
 
 void send_request_to_shut_down(zmq::socket_t &sock) {
   json generate_shutdown_request = {{"type", SHUT_DOWN}};
